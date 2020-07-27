f9288205171d27443877caca4071d924b40b7af8
wups0713 <68268720+wups0713@users.noreply.github.com>
2020-07-27 10:07:13 +0800

    Update Edit_house.php

--- database_webpage/Edit_house.php
+++ database_webpage/Edit_house.php
@@ -22,7 +22,7 @@
             $db_host = "dbhome.cs.nctu.edu.tw";
             $db_name = "wups0713_cs_HW3";
             $db_user = "wups0713_cs";
-            $db_password = "aa066889";
+            $db_password = "********";
             $db = new mysqli ($db_host, $db_user, $db_password, $db_name);
             $id = $_POST["id"];
             $sql = $db->query ("SELECT max(information_id) AS c
